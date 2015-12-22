e0f49bc5682d89a29f50702d25ac10fca66981f0
Rick James <admin@autism.cool>
2015-12-22 02:13:45 -0800

    removed extraneous directory

--- fe/set-domain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then
-  echo "Usage: ./set-domain.sh yourdomain. The string 'localhost:8080' will be replaced by yourdomain on all files."
-else
-  find . -type f \( -name "*.css" -o -name "*.html" \) -exec sed -i "s/localhost:8080/$1/g" {} +
-fi
-
-
--- fe/static/settings.js
+++
@@ -1,3 +0,0 @@
-var VERBOSE = true;
-var DISABLE_JS = false;
-var API_DOMAIN = 'http://api.hambubger.com/';
