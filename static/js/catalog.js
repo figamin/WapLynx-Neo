0509fb594bf0b9b96ae543ade533ef63d346f45c
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2016-10-11 13:18:51 -0300

    Now hidden threads are also hidden on the catalog.

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -14,4 +14,33 @@ if (!DISABLE_JS) {
     };
   }
 
+  var links = document.getElementsByClassName('linkThumb');
+
+  var storedHidingData = localStorage.hidingData;
+
+  if (storedHidingData) {
+    storedHidingData = JSON.parse(storedHidingData);
+  } else {
+    storedHidingData = {};
+  }
+
+  for (var i = 0; i < links.length; i++) {
+
+    var link = links[i];
+
+    var matches = link.href.match(/(\w+)\/res\/(\d+)/);
+
+    var board = matches[1];
+    var thread = matches[2];
+
+    var boardData = storedHidingData[board];
+
+    if (boardData && boardData.threads.indexOf(thread) > -1) {
+      var cell = link.parentNode;
+
+      cell.parentNode.removeChild(cell);
+    }
+
+  }
+
 }
