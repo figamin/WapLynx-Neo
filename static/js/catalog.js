d7cc89dd98ad2a1356afbd9815e0b2e8d60b2968
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2016-12-12 20:51:45 -0200

    Implemented search on the catalog.

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,5 +1,50 @@
 if (!DISABLE_JS) {
 
+  var indicatorsRelation = {
+    pinned : 'pinIndicator',
+    locked : 'lockIndicator',
+    cyclic : 'cyclicIndicator',
+    autoSage : 'bumpLockIndicator'
+  };
+
+  var catalogCellTemplate = '<a class="linkThumb"></a>';
+  catalogCellTemplate += '<p class="threadStats">R:';
+  catalogCellTemplate += '<span class="labelReplies"></span>';
+  catalogCellTemplate += '/ I:';
+  catalogCellTemplate += '<span class="labelImages"></span>';
+  catalogCellTemplate += '/ P:';
+  catalogCellTemplate += '<span class="labelPage"></span>';
+  catalogCellTemplate += '<span class="lockIndicator" title="Locked"></span>';
+  catalogCellTemplate += '<span class="pinIndicator" title="Sticky"></span>';
+  catalogCellTemplate += '<span class="cyclicIndicator" title="Cyclical Thread"></span>';
+  catalogCellTemplate += '<span class="bumpLockIndicator" title="Bumplocked"></span>';
+  catalogCellTemplate += '</p>';
+  catalogCellTemplate += '<p><span class="labelSubject"></span></p>';
+  catalogCellTemplate += '<div class="divMessage"></div>';
+
+  var catalogDiv = document.getElementById('divThreads');
+  var loadingData = false;
+  var searchTimer;
+  var catalogThreads;
+
+  var boardUri = window.location.toString().match(/\/(\w+)\/catalog.html/)[1];
+
+  document.getElementById('divTools').style.display = 'inline-block';
+
+  var searchField = document.getElementById('catalogSearchField');
+
+  searchField.addEventListener('input', function() {
+
+    if (searchTimer) {
+      clearTimeout(searchTimer);
+    }
+
+    searchTimer = setTimeout(function() {
+      search(searchField.value);
+    }, 1000);
+
+  });
+
   var postingForm = document.getElementById('newPostFieldset');
 
   if (postingForm) {
@@ -42,5 +87,113 @@ if (!DISABLE_JS) {
     }
 
   }
+
+  getCatalogData();
+
+}
+
+function removeElement(element) {
+  element.parentNode.removeChild(element);
+}
+
+function setCellThumb(thumbLink, thread) {
+  thumbLink.href = '/' + boardUri + '/res/' + thread.threadId + '.html';
+
+  if (thread.thumb) {
+    var thumbImage = document.createElement('img');
+
+    thumbImage.src = thread.thumb;
+    thumbLink.appendChild(thumbImage);
+  } else {
+    thumbLink.innerHTML = 'Open';
+  }
+}
+
+function setCatalogCellIndicators(thread, cell) {
+
+  for ( var key in indicatorsRelation) {
+    if (!thread[key]) {
+      removeElement(cell.getElementsByClassName(indicatorsRelation[key])[0]);
+    }
+  }
+
+}
+
+function setCell(thread) {
+
+  var cell = document.createElement('div');
+
+  cell.innerHTML = catalogCellTemplate;
+  cell.setAttribute('class', 'catalogCell');
+
+  setCellThumb(cell.getElementsByClassName('linkThumb')[0], thread);
+
+  var labelReplies = cell.getElementsByClassName('labelReplies')[0];
+  labelReplies.innerHTML = thread.postCount || 0;
+
+  var labelImages = cell.getElementsByClassName('labelImages')[0];
+  labelImages.innerHTML = thread.fileCount || 0;
+  cell.getElementsByClassName('labelPage')[0].innerHTML = thread.page;
+
+  if (thread.subject) {
+    cell.getElementsByClassName('labelSubject')[0].innerHTML = thread.subject;
+  }
+
+  setCatalogCellIndicators(thread, cell);
+
+  cell.getElementsByClassName('divMessage')[0].innerHTML = thread.message;
+
+  return cell;
+
+}
+
+function search(term) {
+
+  if (!catalogThreads) {
+    return;
+  }
+
+  term = term.toLowerCase();
+
+  while (catalogDiv.firstChild) {
+    catalogDiv.removeChild(catalogDiv.firstChild);
+  }
+
+  for (var i = 0; i < catalogThreads.length; i++) {
+
+    var thread = catalogThreads[i];
+
+    if (term.length && thread.message.toLowerCase().indexOf(term) < 0
+        && (thread.subject || '').toLowerCase().indexOf(term) < 0) {
+      continue;
+    }
+
+    catalogDiv.appendChild(setCell(thread));
+
+  }
+
+}
+
+function getCatalogData() {
+
+  if (loadingData) {
+    return;
+  }
+
+  loadingData = true;
+
+  localRequest('/' + boardUri + '/catalog.json', function gotBoardData(error,
+      data) {
+
+    loadingData = false;
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    catalogThreads = JSON.parse(data);
+
+  });
 
 }
