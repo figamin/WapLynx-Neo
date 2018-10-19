2075c63ad94f2bf2f79a65f7c7f425718aaa2034
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2018-10-19 17:47:00 -0300

    Added auto refresh for latest postings.

---
+++ static/js/latestPostings.js
@@ -0,0 +1,67 @@
+var latestCheck;
+var postsDiv;
+var unread = 0;
+var originalTitle = document.title;
+
+if (!DISABLE_JS) {
+
+  postsDiv = document.getElementById('divPostings');
+  latestCheck = new Date();
+  startTimer();
+
+  document.addEventListener('visibilitychange', function changed() {
+
+    if (unread && !document.hidden) {
+      unread = 0;
+      document.title = originalTitle;
+    }
+
+  }, false);
+
+}
+
+function startTimer() {
+
+  setTimeout(function refresh() {
+
+    latestCheck = new Date();
+
+    localRequest('/latestPostings.js?json=1&date=' + latestCheck.toUTCString()
+        + '&boards=' + document.getElementById('fieldBoards').value,
+        function gotData(error, data) {
+
+          startTimer();
+
+          if (!data) {
+            return;
+          }
+
+          data = JSON.parse(data);
+
+          if (document.hidden) {
+            unread += data.length;
+
+            if (!unread) {
+              return;
+            }
+
+            document.title = originalTitle + '(' + unread + ')';
+          }
+
+          for (var i = 0; i < data.length; i++) {
+
+            var post = data[i];
+
+            var cell = addPost(post, post.boardUri, post.threadId, true);
+
+            cell.getElementsByClassName('deletionCheckBox')[0].remove();
+
+            postsDiv.insertBefore(cell, postsDiv.childNodes[0]);
+
+          }
+
+        });
+
+  }, 1000 * 60);
+
+}
