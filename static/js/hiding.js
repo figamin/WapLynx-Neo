e2b036730565c9b852d97a968a1744ab5b624f20
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2019-04-09 12:57:21 -0300

    Fixed hiding not applying on page loading.

--- static/js/hiding.js
+++ static/js/hiding.js
@@ -165,23 +165,55 @@ hiding.registerHiding = function(board, thread, post, unhiding) {
 
 };
 
-hiding.buildHideMenu = function(linkSelf, hideMenu) {
+hiding.hidePost = function(linkSelf, board, thread, post) {
 
-  var href = linkSelf.href;
+  hiding.toggleHidden(linkSelf.parentNode.parentNode, true);
 
-  var parts = href.split('/');
+  hiding.registerHiding(board, thread, post);
 
-  var board = parts[3];
+  var unhidePostButton = document.createElement('span');
 
-  var finalParts = parts[5].split('.');
+  var unhideHTML = '[Unhide ' + (post ? 'post' : 'OP') + ' ' + board + '/'
+      + post + ']';
 
-  var thread = finalParts[0];
+  unhidePostButton.innerHTML = unhideHTML;
+  unhidePostButton.className = 'unhideButton glowOnHover';
 
-  var post = finalParts[1].split('#')[1];
+  linkSelf.parentNode.parentNode.parentNode.insertBefore(unhidePostButton,
+      linkSelf.parentNode.parentNode);
 
-  if (post === thread) {
-    post = undefined;
+  unhidePostButton.onclick = function() {
+
+    hiding.registerHiding(board, thread, post, true);
+    unhidePostButton.remove();
+
+    hiding.toggleHidden(linkSelf.parentNode.parentNode, false);
+
+  };
+
+};
+
+hiding.hideThread = function(linkSelf, board, thread) {
+
+  hiding.toggleHidden(linkSelf.parentNode.parentNode.parentNode, true);
+  var unhideThreadButton = document.createElement('span');
+
+  unhideThreadButton.innerHTML = '[Unhide thread ' + board + '/' + thread + ']';
+  unhideThreadButton.className = 'unhideButton glowOnHover';
+  linkSelf.parentNode.parentNode.parentNode.parentNode.insertBefore(
+      unhideThreadButton, linkSelf.parentNode.parentNode.parentNode);
+
+  hiding.registerHiding(board, thread);
+
+  unhideThreadButton.onclick = function() {
+    hiding.toggleHidden(linkSelf.parentNode.parentNode.parentNode, false);
+    unhideThreadButton.remove();
+    hiding.registerHiding(board, thread, null, true);
   }
+
+};
+
+hiding.buildHideMenu = function(board, thread, post, linkSelf, hideMenu) {
 
   var postHideButton;
   postHideButton = document.createElement('div');
@@ -236,74 +268,16 @@ hiding.buildHideMenu = function(linkSelf, hideMenu) {
     hideMenu.appendChild(document.createElement('hr'));
   }
 
-  var unhidePostButton;
-
   postHideButton.onclick = function() {
-
-    hiding.toggleHidden(linkSelf.parentNode.parentNode, true);
-
-    hiding.registerHiding(board, thread, post || thread);
-
-    unhidePostButton = document.createElement('span');
-
-    var unhideHTML = '[Unhide ' + (post ? 'post' : 'OP') + ' ' + board + '/'
-        + (post || thread) + ']';
-
-    unhidePostButton.innerHTML = unhideHTML;
-    unhidePostButton.className = 'unhideButton glowOnHover';
-
-    linkSelf.parentNode.parentNode.parentNode.insertBefore(unhidePostButton,
-        linkSelf.parentNode.parentNode);
-
-    unhidePostButton.onclick = function() {
-
-      hiding.registerHiding(board, thread, post || thread, true);
-      unhidePostButton.remove();
-      hiding.toggleHidden(linkSelf.parentNode.parentNode, false);
-
-    };
+    hiding.hidePost(linkSelf, board, thread, post || thread);
   };
 
   if (!post) {
 
-    var unhideThreadButton;
-
     threadHideButton.onclick = function() {
-      hiding.toggleHidden(linkSelf.parentNode.parentNode.parentNode, true);
-      unhideThreadButton = document.createElement('span');
-
-      unhideThreadButton.innerHTML = '[Unhide thread ' + board + '/' + thread
-          + ']';
-      unhideThreadButton.className = 'unhideButton glowOnHover';
-      linkSelf.parentNode.parentNode.parentNode.parentNode.insertBefore(
-          unhideThreadButton, linkSelf.parentNode.parentNode.parentNode);
-
-      hiding.registerHiding(board, thread);
-
-      unhideThreadButton.onclick = function() {
-        hiding.toggleHidden(linkSelf.parentNode.parentNode.parentNode, false);
-        unhideThreadButton.remove();
-        hiding.registerHiding(board, thread, null, true);
-      }
-
+      hiding.hideThread(linkSelf, board, thread);
     }
 
-  }
-
-  hiding.checkFilterHiding(linkSelf);
-
-  var boardData = hiding.storedHidingData[board];
-
-  if (!boardData) {
-    return;
-  }
-
-  if (boardData.posts.indexOf(post || thread) > -1) {
-    postHideButton.onclick();
-  }
-
-  if (!post && boardData.threads.indexOf(thread) > -1) {
-    threadHideButton.onclick();
   }
 
 };
@@ -330,6 +304,22 @@ hiding.setHideMenu = function(linkSelf) {
 
   var checkbox = parentNode.getElementsByClassName('deletionCheckBox')[0];
 
+  var href = linkSelf.href;
+
+  var parts = href.split('/');
+
+  var board = parts[3];
+
+  var finalParts = parts[5].split('.');
+
+  var thread = finalParts[0];
+
+  var post = finalParts[1].split('#')[1];
+
+  if (post === thread) {
+    post = undefined;
+  }
+
   parentNode.insertBefore(hideButton, checkbox ? checkbox.nextSibling
       : parentNode.childNodes[0]);
 
@@ -355,9 +345,25 @@ hiding.setHideMenu = function(linkSelf) {
 
     hiding.shownMenu = hideMenu;
 
-    hiding.buildHideMenu(linkSelf, hideMenu);
+    hiding.buildHideMenu(board, thread, post, linkSelf, hideMenu);
 
   };
+
+  var boardData = hiding.storedHidingData[board];
+
+  if (!boardData) {
+    return;
+  }
+
+  if (boardData.posts.indexOf(post || thread) > -1) {
+    hiding.hidePost(linkSelf, board, thread, post || thread);
+  }
+
+  if (!post && boardData.threads.indexOf(thread) > -1) {
+    hiding.hideThread(linkSelf, board, thread);
+  }
+
+  hiding.checkFilterHiding(linkSelf);
 
 };
 
