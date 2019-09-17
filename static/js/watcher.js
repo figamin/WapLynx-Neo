924573d178a7a2394d19650cc37924be8354a3c1
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2019-09-17 15:23:05 -0300

    Now the mobile watcher button turns red when there are new replies on watched threads.

--- static/js/watcher.js
+++ static/js/watcher.js
@@ -113,6 +113,15 @@ watcher.init = function() {
 };
 
 watcher.updateWatcherCounter = function() {
+
+  var classList = watcherButton.classList;
+
+  if (watcher.watcherAlertCounter) {
+    classList.add('mobileAlert');
+  } else {
+    classList.remove('mobileAlert');
+  }
+
   watcher.watcherCounter.innerHTML = watcher.watcherAlertCounter ? '('
       + watcher.watcherAlertCounter + ')' : '';
 };
