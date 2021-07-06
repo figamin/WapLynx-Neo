bda84711afc58465a9c87a3de516005bfe0935d4
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2021-07-06 13:28:45 -0300

    Fixed merge button on posting menu.

--- static/js/postingMenu.js
+++ static/js/postingMenu.js
@@ -705,8 +705,8 @@ postingMenu.setExtraMenuThread = function(extraMenu, board, thread, innerPart) {
   extraMenu.appendChild(document.createElement('hr'));
 
   var mergeButton = document.createElement('div');
-  archiveButton.innerHTML = 'Merge';
-  archiveButton.onclick = function() {
+  mergeButton.innerHTML = 'Merge';
+  mergeButton.onclick = function() {
     postingMenu.mergeThread(board, thread);
   };
   extraMenu.appendChild(mergeButton);
